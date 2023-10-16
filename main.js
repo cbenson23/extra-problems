8aa907720c2f703bf15fa4cf4bc0d4e8e583e654
ChrisBenson <cbenson.prsvr@gmail.com>
2023-10-16 10:30:12 -0500

    start

---
+++ main.js
@@ -0,0 +1,60 @@
+function isEvenlyDivisible() {
+
+}
+
+function halfSquare() {
+}
+
+function isLong() {
+}
+
+function exclaim() {
+}
+
+function countWords() {
+}
+
+function containsDigit(str) {
+}
+
+function containsLowerCase() {
+}
+
+function containsUpperCase() {
+}
+
+function containsNonAlphanumeric() {
+}
+
+function containsSpace() {
+}
+
+function digits(num) {
+}
+
+function truncate() {
+}
+
+function isValidPassword() {
+}
+
+function onlyPunchy() {
+}
+
+
+module.exports = {
+  isEvenlyDivisible,
+  halfSquare,
+  exclaim,
+  isLong,
+  containsDigit,
+  containsLowerCase,
+  containsUpperCase,
+  containsNonAlphanumeric,
+  containsSpace,
+  countWords,
+  digits,
+  truncate,
+  isValidPassword,
+  onlyPunchy,
+}
